ad0912f1fef3ddb92c67f6b00c08b32673fbf39b
ziruihao <ziray.hao.22@dartmouth.edu>
2019-11-19 01:49:21 -0500

    updated comments for the MLP class

--- mlp/.py
+++ mlp/.py
@@ -39,26 +39,29 @@ test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=True
 class MLP(torch.nn.Module):
 
     def __init__(self, input_size, output_size, hidden_layer_sizes):
-        print(hidden_layer_sizes)
         super(MLP, self).__init__()
-        construct_layers = []
+
+        # the layers that define the network
+        layers = []
+        
         prev_layer_size = input_size
         
         for i, hidden_layer_size in enumerate(hidden_layer_sizes):
-            construct_layers.append((str(i) + 'linear', torch.nn.Linear(prev_layer_size, hidden_layer_size)))
-            construct_layers.append((str(i) + 'activation', torch.nn.ReLU()))
+            layers.append((str(i) + 'linear', torch.nn.Linear(prev_layer_size, hidden_layer_size)))
+            # we have a non-linear ReLU activation layer after each linear
+            layers.append((str(i) + 'activation', torch.nn.ReLU()))
             prev_layer_size = hidden_layer_size
-        construct_layers.append((str(len(hidden_layer_sizes)) + 'linear', torch.nn.Linear(prev_layer_size, output_size)))
-        construct_layers.append(('final activation', torch.nn.LogSoftmax(dim = 1)))
 
-        for layer in construct_layers:
-            print(layer)
+        layers.append((str(len(hidden_layer_sizes)) + 'linear', torch.nn.Linear(prev_layer_size, output_size)))
+        # we have a final log(Softmax) function to find the most likely prediction in the last layer
+        layers.append(('final activation', torch.nn.LogSoftmax(dim = 1)))
 
-        self.layers = torch.nn.Sequential(OrderedDict(construct_layers))
+        self.layers = torch.nn.Sequential(OrderedDict(layers))
+
     def forward(self, x):
         return self.layers(x)
 
-model = MLP(784, 10, [600, 500, 400, 300, 200, 100])
+model = MLP(784, 10, [300])
 
 loss_function = torch.nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
