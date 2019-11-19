b5dc5025f0640b0d04db89dad44a2819421f4aec
ziruihao <ziray.hao.22@dartmouth.edu>
2019-11-19 16:24:29 -0500

    updated algo

--- mlp/run.py
+++ mlp/run.py
@@ -1,37 +1,147 @@
-import numpy as np
 import torchvision
 import torch
 from mlp import MLP
 from slp import SLP
 from train import train
+from test import test
+import csv
 
 transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.5,), (0.5,))])
 
 train_data = torchvision.datasets.MNIST('PATH_TO_STORE_TRAINSET', download=True, train=True, transform=transform)
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
 
-model = MLP(784, 10, [300])
-print('training for 3 epochs')
-train('MLP_1_NLL_SGD_3', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 3, transform, train_data, train_loader)
-print('training for 10 epochs')
-train('MLP_1_NLL_SGD_10', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 10, transform, train_data, train_loader)
-print('training for 30 epochs')
-train('MLP_1_NLL_SGD_30', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 30, transform, train_data, train_loader)
+test_data = torchvision.datasets.MNIST('PATH_TO_STORE_TESTSET', download=True, train=False, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=True)
 
-model = MLP(784, 10, [400, 200])
-print('training for 3 epochs')
-train('MLP_2_NLL_SGD_3', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 3, transform, train_data, train_loader)
-print('training for 10 epochs')
-train('MLP_2_NLL_SGD_10', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 10, transform, train_data, train_loader)
-print('training for 30 epochs')
-train('MLP_2_NLL_SGD_30', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 30, transform, train_data, train_loader)
+def train_all():
 
-model = MLP(784, 10, [600, 500, 400, 300, 200, 100])
-print('training for 3 epochs')
-train('MLP_6_NLL_SGD_3', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 3, transform, train_data, train_loader)
-print('training for 10 epochs')
-train('MLP_6_NLL_SGD_10', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 10, transform, train_data, train_loader)
-print('training for 30 epochs')
-train('MLP_6_NLL_SGD_30', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 30, transform, train_data, train_loader)
+    # NLLLoss and LogSoftmax
 
+    model = MLP(784, 10, [300])
+    print('training for 3 epochs')
+    train('MLP_1_NLL_SGD_3', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 3, transform, train_data, train_loader)
+    print('training for 10 epochs')
+    train('MLP_1_NLL_SGD_10', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 10, transform, train_data, train_loader)
+    print('training for 30 epochs')
+    train('MLP_1_NLL_SGD_30', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 30, transform, train_data, train_loader)
 
+    model = MLP(784, 10, [400, 200])
+    print('training for 3 epochs')
+    train('MLP_2_NLL_SGD_3', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 3, transform, train_data, train_loader)
+    print('training for 10 epochs')
+    train('MLP_2_NLL_SGD_10', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 10, transform, train_data, train_loader)
+    print('training for 30 epochs')
+    train('MLP_2_NLL_SGD_30', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 30, transform, train_data, train_loader)
+
+    model = MLP(784, 10, [600, 500, 400, 300, 200, 100])
+    print('training for 3 epochs')
+    train('MLP_6_NLL_SGD_3', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 3, transform, train_data, train_loader)
+    print('training for 10 epochs')
+    train('MLP_6_NLL_SGD_10', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 10, transform, train_data, train_loader)
+    print('training for 30 epochs')
+    train('MLP_6_NLL_SGD_30', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 30, transform, train_data, train_loader)
+
+    # CrossEntropyLoss and Softmax
+
+    model = MLP(784, 10, [300])
+    print('training for 3 epochs')
+    train('MLP_1_CEL_SGD_3', model, torch.nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 3, transform, train_data, train_loader)
+    print('training for 10 epochs')
+    train('MLP_1_CEL_SGD_10', model, torch.nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 10, transform, train_data, train_loader)
+    print('training for 30 epochs')
+    train('MLP_1_CEL_SGD_30', model, torch.nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 30, transform, train_data, train_loader)
+
+    model = MLP(784, 10, [400, 200])
+    print('training for 3 epochs')
+    train('MLP_2_CEL_SGD_3', model, torch.nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 3, transform, train_data, train_loader)
+    print('training for 10 epochs')
+    train('MLP_2_CEL_SGD_10', model, torch.nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 10, transform, train_data, train_loader)
+    print('training for 30 epochs')
+    train('MLP_2_CEL_SGD_30', model, torch.nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 30, transform, train_data, train_loader)
+
+    model = MLP(784, 10, [600, 500, 400, 300, 200, 100])
+    print('training for 3 epochs')
+    train('MLP_6_CEL_SGD_3', model, torch.nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 3, transform, train_data, train_loader)
+    print('training for 10 epochs')
+    train('MLP_6_CEL_SGD_10', model, torch.nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 10, transform, train_data, train_loader)
+    print('training for 30 epochs')
+    train('MLP_6_CEL_SGD_30', model, torch.nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 30, transform, train_data, train_loader)
+
+    # 100 epochs with 8 hidden layers
+
+    model = MLP(784, 10, [64, 64, 64, 64, 64, 64, 64, 64])
+    print('training for 100 epochs')
+    train('MLP_8_CEL_SGD', model, torch.nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 100, transform, train_data, train_loader)
+
+def test_all():
+
+    with open('performance.csv', mode='w') as performance:
+
+        write = csv.writer(performance, delimiter=',', quotechar='"')
+
+        # NLLLoss and LogSoftmax
+
+        # model = MLP(784, 10, [300])
+        # print('testing for 3 epochs')
+        # write.writerow(['MLP_1_NLL_SGD_3', test('MLP_1_NLL_SGD_3', model, test_data, test_loader)])
+        # print('testing for 10 epochs')
+        # write.writerow(['MLP_1_NLL_SGD_10', test('MLP_1_NLL_SGD_10', model, test_data, test_loader)])
+        # print('testing for 30 epochs')
+        # write.writerow(['MLP_1_NLL_SGD_30', test('MLP_1_NLL_SGD_30', model, test_data, test_loader)])
+
+        # model = MLP(784, 10, [400, 200])
+        # print('testing for 3 epochs')
+        # write.writerow(['MLP_2_NLL_SGD_3', test('MLP_2_NLL_SGD_3', model, test_data, test_loader)])
+        # print('testing for 10 epochs')
+        # write.writerow(['MLP_2_NLL_SGD_10', test('MLP_2_NLL_SGD_10', model,test_data, test_loader)])
+        # print('testing for 30 epochs')
+        # write.writerow(['MLP_2_NLL_SGD_30', test('MLP_2_NLL_SGD_30', model, test_data, test_loader)])
+
+        # model = MLP(784, 10, [600, 500, 400, 300, 200, 100])
+        # print('testing for 3 epochs')
+        # write.writerow(['MLP_6_NLL_SGD_3', test('MLP_6_NLL_SGD_3', model, test_data, test_loader)])
+        # print('testing for 10 epochs')
+        # write.writerow(['MLP_6_NLL_SGD_10', test('MLP_6_NLL_SGD_10', model, test_data, test_loader)])
+        # print('testing for 30 epochs')
+        # write.writerow(['MLP_6_NLL_SGD_30', test('MLP_6_NLL_SGD_30', model, test_data, test_loader)])
+
+        # # CrossEntropyLoss and Softmax
+
+        # model = MLP(784, 10, [300])
+        # print('testing for 3 epochs')
+        # write.writerow(['MLP_1_CEL_SGD_3', test('MLP_1_CEL_SGD_3', model, test_data, test_loader)])
+        # print('testing for 10 epochs')
+        # write.writerow(['MLP_1_CEL_SGD_10', test('MLP_1_CEL_SGD_10', model, test_data, test_loader)])
+        # print('testing for 30 epochs')
+        # write.writerow(['MLP_1_CEL_SGD_30', test('MLP_1_CEL_SGD_30', model, test_data, test_loader)])
+
+        # model = MLP(784, 10, [400, 200])
+        # print('testing for 3 epochs')
+        # write.writerow(['MLP_2_CEL_SGD_3', test('MLP_2_CEL_SGD_3', model, test_data, test_loader)])
+        # print('testing for 10 epochs')
+        # write.writerow(['MLP_2_CEL_SGD_10', test('MLP_2_CEL_SGD_10', model, test_data, test_loader)])
+        # print('testing for 30 epochs')
+        # write.writerow(['MLP_2_CEL_SGD_30', test('MLP_2_CEL_SGD_30', model, test_data, test_loader)])
+
+        model = MLP(784, 10, [600, 500, 400, 300, 200, 100])
+        # # print('testing for 3 epochs')
+        write.writerow(['MLP_6_CEL_SGD_3', test('MLP_6_CEL_SGD_3', model, test_data, test_loader)])
+        # # print('testing for 10 epochs')
+        write.writerow(['MLP_6_CEL_SGD_10', test('MLP_6_CEL_SGD_10', model, test_data, test_loader)])
+        # print('testing for 30 epochs')
+        write.writerow(['MLP_6_CEL_SGD_30', test('MLP_6_CEL_SGD_30', model, test_data, test_loader)])
+
+        # 100 epochs with 8 hidden layers
+
+        # model = MLP(784, 10, [64, 64, 64, 64, 64, 64, 64, 64])
+        # print('testing for 100 epochs')
+        # for epoch in range(100):
+        #     if ((epoch % 5) is 0):
+        #         write.writerow(['MLP_8_CEL_SGD_' + str(epoch), test('MLP_8_CEL_SGD_' + str(epoch), model, test_data, test_loader)])
+
+    performance.close()
+
+model = SLP(784, 10)
+train('SLP_NLL_SGD_30', model, torch.nn.NLLLoss(), torch.optim.SGD(model.parameters(), lr = 0.001), 30, transform, train_data, train_loader)
+test('SLP_NLL_SGD_30', model, test_data, test_loader)
--- mlp/test.py
+++ mlp/test.py
@@ -1,48 +1,31 @@
 import torch
 import numpy as np
-import torchvision
-from torchvision import datasets, transforms
-import sys
 import math
 from mlp import MLP
 from slp import SLP
 
-model_name = str(sys.argv[1])
+def test(model_name, model, test_data, test_loader):
 
-transform = transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.5,), (0.5,))])
+  model.load_state_dict(torch.load('./models/' + model_name + '.pt'))
 
-test_data = torchvision.datasets.MNIST('PATH_TO_STORE_TESTSET', download=True, train=False, transform=transform)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=True)
+  # configures the model to 'evaluation' mode, which ensures that steps are not logged
+  model.eval()
 
-# model = MLP(784, 10, [400, 200])
-model = SLP(784, 10)
+  # checker code from https://towardsdatascience.com/handwritten-digit-mnist-pytorch-977b5338e627
+  correct_count, all_count = 0, 0
+  for test_digits, test_labels in test_loader:
+    for i in range(len(test_labels)):
+      img = test_digits[i].view(1, 784)
+      with torch.no_grad():
+          logps = model(img)
+      
+      ps = torch.exp(logps)
+      probab = list(ps.numpy()[0])
+      pred_label = probab.index(max(probab))
+      true_label = test_labels.numpy()[i]
+      if(true_label == pred_label):
+        correct_count += 1
+      all_count += 1
 
-model.load_state_dict(torch.load('./models/' + model_name + '.pt'))
-
-print(model.summary())
-
-# configures the model to 'evaluation' mode, which ensures that steps are not logged
-model.eval()
-
-# checker code from https://towardsdatascience.com/handwritten-digit-mnist-pytorch-977b5338e627
-correct_count, all_count = 0, 0
-for test_digits,test_labels in test_loader:
-  for i in range(len(test_labels)):
-    img = test_digits[i].view(1, 784)
-    with torch.no_grad():
-        logps = model(img)
-
-    
-    ps = torch.exp(logps)
-    probab = list(ps.numpy()[0])
-    pred_label = probab.index(max(probab))
-    true_label = test_labels.numpy()[i]
-    if(true_label == pred_label):
-      correct_count += 1
-    all_count += 1
-
-print("Number Of Images Tested =", all_count)
-print("\nModel Accuracy =", (correct_count/all_count))
-
-print("Number Of Images Tested =", all_count)
-print("\nModel Accuracy =", (correct_count/all_count))
+  print("\nModel Accuracy =", (correct_count/all_count))
+  return (correct_count/all_count)
